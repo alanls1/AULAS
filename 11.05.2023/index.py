4105b402e351066a332cdaf35c8f864a3bc70344
alan lima <alanlima4123@gmail.com>
2023-05-15 18:47:29 -0300

    realização de alguns exercícios simples de lógica de programação, utilizando Pynthon

--- 11.05.2023/index.py
+++ 11.05.2023/index.py
@@ -1,6 +1,22 @@
 import math
 
-raio = int(input("Raio: "))
-resultado = 4/3 * math.pi * math.pow(raio, 3)
+#exem
+ 
+numero = int(input("Digite um valor: "))
+resultado = 4/3 * math.pi * math.pow(numero, 3)
 
-print ( resultado )
+print("resultado:", resultado )
+#Fórmula com o módulo e o numero
+
+
+
+#exer 2
+
+esfera = 4 * math.pi * math.pow(numero, 2)
+
+print ("esfera: ",esfera)
+
+#exer 3 
+
+
+print(math.pow(numero, 2), math.pow(numero, 3), math.pow(numero, 4))
